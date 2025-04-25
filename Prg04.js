cd710bb4a8dcada9feac2d339b0ec93906a194d9
Tarun-Deshmukh01 <tarunbit2002@gmail>
2025-04-25 16:52:59 +0530

    1st commit

---
+++ Prg04.js
@@ -0,0 +1,19 @@
+//Defining a function for finding interest amount
+
+function calcAmount(){
+    let p,t,r,im,ta;  // local variable
+
+    // Data Collecting
+    p =+pamt.value;
+    t =+ time.value;
+    r =+ roi.value;
+
+    //calculation
+    ia = p*r*t/100;
+    ta = p+ ia
+
+    //displaying
+    intAmt.innerHTML="Interest Amount &nbsp; &#8377;"+ia;
+    totAmt.innerHTML="Total Amount &nbsp; &#8377;"+ta;
+
+}
